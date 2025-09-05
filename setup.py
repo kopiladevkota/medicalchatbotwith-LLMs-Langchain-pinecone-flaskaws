bad13e60264b5eeed26a5ba191061d2e8035fb4f
Kopila Devkota <kopiladevkota7@gmail.com>
2025-09-05 11:16:07 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="medicalchatbot",
+    version="0.1.0",
+    author="Kopila Devkota",
+    author_email="kopiladevkota7@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
